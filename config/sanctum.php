3831ff8bda278a67e7f2b97d15e92ee78853a332
Arwa Kassab <arwariad22@gmail.com>
2025-09-22 01:05:14 +0300

    edit

--- config/sanctum.php
+++ config/sanctum.php
@@ -77,7 +77,9 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'verify_csrf_token' => App\Http\Middleware\VerifyTempToken::class,
+//        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+
     ],
 
 ];
