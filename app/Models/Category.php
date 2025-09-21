94ec4e06c9d2a05920b4b262b70abaf0d7ef8ded
Arwa Kassab <arwariad22@gmail.com>
2025-09-21 20:23:00 +0300

    seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
     use SoftDeletes;
-    protected $fillable = [
-        'name',
-    ];
+    protected $fillable = ['name', 'image'];
+
+
+    protected $appends = ['image'];
 
     public function stores()
     {
         return $this->belongsToMany(User::class, 'store_category', 'category_id', 'store_id');
+    }
+    public function getImageAttribute($value)
+    {
+
+        return $value ? Storage::url($value) : null;
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,30 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
 
 class CategorySeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
-        // تخزين الصورة في مجلد categories
-        $imagePath = storage_path('app/public/categories'); // مسار التخزين المحلي
-        if (!File::exists($imagePath)) {
-            File::makeDirectory($imagePath, 0777, true); // إنشاء المجلد إذا لم يكن موجود
-        }
-
-        // مثال لتحميل صورة من مسار محلي أو URL
-        $image = file_get_contents(public_path('images/category_image.jpg'));  // قم بتغيير المسار حسب الصورة الفعلية لديك
-        $imageName = 'category_image.jpg';
-        Storage::put('public/categories/' . $imageName, $image);  // تخزين الصورة في مجلد categories
-
-        // إضافة الفئات مع مسار الصورة
         DB::table('categories')->insert([
-            ['name' => 'معجنات', 'image' => 'storage/categories/' . $imageName],
-            ['name' => 'خضار', 'image' => 'storage/categories/' . $imageName],
-            ['name' => 'مواد غذائية', 'image' => 'storage/categories/' . $imageName],
+            [
+                'name' => 'مواد غذائية',
+                'image' => 'categories/غذائية.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'خضار',
+                'image' => 'categories/خضار.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'معجنات',
+                'image' => 'categories/معجنات.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
-
