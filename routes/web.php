88cc6f4959ce295568531fdfdab8373b8abcf76f
Arwa Kassab <arwariad22@gmail.com>
2025-10-08 16:34:55 +0300

    Move Firebase web config to /firebase-config.js + secure keys

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/firebase-config.js', function () {
+    $cfg = [
+        'config' => [
+            'apiKey'            => config('services.firebase_web.api_key'),
+            'authDomain'        => config('services.firebase_web.auth_domain'),
+            'projectId'         => config('services.firebase_web.project_id'),
+            'messagingSenderId' => config('services.firebase_web.sender_id'),
+            'appId'             => config('services.firebase_web.app_id'),
+        ],
+        'vapidPublicKey' => config('services.firebase_web.vapid_public_key'),
+    ];
+
+    return response('self._FIREBASE = '.json_encode($cfg, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE).';')
+        ->header('Content-Type','application/javascript')
+        ->header('Cache-Control','no-store, no-cache, must-revalidate');
+});
+
